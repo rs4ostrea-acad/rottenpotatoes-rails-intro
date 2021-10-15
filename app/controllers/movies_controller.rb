1476e3e009dfe284a7c458dc8ec5746c7b9004d0
Rafael Ostrea <rs4ostrea@berkeley.edu>
2021-10-15 09:07:03 +0000

    starting over because the autograder makes no sense

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,40 +8,6 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
-    @all_ratings = Movie.all_ratings
-    @ratings_to_show = []
-    
-    
-    if !params[:ratings].nil?
-      params[:ratings].each_key do |key|
-        @ratings_to_show << key
-      end
-    elsif !session[:ratings].nil?
-      session[:ratings].each_key do |key| 
-        @ratings_to_show << key 
-      end
-    end
-    
-    @movies = Movie.with_ratings(@ratings_to_show)
-    @ratings_to_show_hash = Hash[@ratings_to_show.collect{|r| [r, 1]}]
-    session[:ratings] = @ratings_to_show_hash
-    
-    
-    if params[:sort] == 'title'
-      @color_title = true
-      @movies = @movies.order(params[:sort])
-    elsif params[:sort] == 'release_date'
-      @color_release_date = true
-      @movies = @movies.order(params[:sort])
-    elsif session[:sort] == 'title'
-      @color_title = true
-      @movies = @movies.order(session[:sort])
-    elsif session[:sort] == 'release_date'
-      @color_release_date = true
-      @movies = @movies.order(session[:sort])
-    end
-    
-    session[:sort] = params[:sort]
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,2 @@
 class Movie < ActiveRecord::Base
-  
-  def self.all_ratings
-    Movie.distinct.pluck(:rating)
-  end
-  
-  def self.with_ratings(included_ratings)
-    if included_ratings.empty?
-      return Movie
-    else
-      return Movie.where(rating: included_ratings)
-    end
-  end
 end
