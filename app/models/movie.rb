643cb97fd6e4bf6734b02cb16cb681327590c5dd
Rafael Ostrea <rs4ostrea@berkeley.edu>
2021-10-15 09:29:06 +0000

    ugh

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    Movie.distinct.pluck(:rating)
+  end
+  
+  def self.with_ratings(included_ratings)
+     Movie.where(rating: included_ratings)
+  end
 end
