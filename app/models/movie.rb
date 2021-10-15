77c44b88ce03a71b4d3f9bef05813fa14b79db13
Rafael Ostrea <rs4ostrea@berkeley.edu>
2021-10-15 09:17:20 +0000

    had to reset everything bc the ag died for some reason

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    Movie.distinct.pluck(:rating)
+  end
+  
+  def self.with_ratings(ratings_list)
+    Movie.where(rating: included_ratings)
+  end
 end
